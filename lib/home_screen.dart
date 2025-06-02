5f0f1acf32831f94e8c3267fc047f03a2255b384
vipin <vipinchowdhary6239@gmail.com>
2025-06-02 18:43:36 +0530

    create ui design

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,31 +3,33 @@ import 'package:demo_project/utils/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key, });
-
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dummyData =Catalog.items[0];
+    final dummyData = Catalog.items[0];
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-
-        title: Text("Home Screen"),),
+      appBar: AppBar(centerTitle: true, title: Text("Home Screen")),
       body: Column(
         children: [
           Card(
-            child: ListTile(leading: Image.network(dummyData.image),
-            title: Text(dummyData.name),
+            elevation: 4,
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+            shadowColor: Colors.black,
+            surfaceTintColor: Colors.deepPurple,
+
+            child: ListTile(
+              leading: Image.network(dummyData.image),
+              title: Text(dummyData.name),
               subtitle: Text(dummyData.desc),
-              trailing:  Text(dummyData.price.toString()),
+              trailing: Text(dummyData.price.toString()),
             ),
           ),
 
         ],
       ),
-       drawer: MyDrawer(),
-floatingActionButton: FloatingActionButton(onPressed: (){},),
+      drawer: MyDrawer(),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
     );
   }
 }
